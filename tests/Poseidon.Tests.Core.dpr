8bcf64c5e7090a9d5a922637f8a1637b08303953
herlonfilgueira <herlonfilgueira@gmail.com>
2026-07-09 16:53:31 -0300

    test(core): runner Poseidon.Tests.Core sem suite de middleware + script de build

---
+++ tests/Poseidon.Tests.Core.dpr
@@ -0,0 +1,50 @@
+program Poseidon.Tests.Core;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System.SysUtils,
+  DUnitX.TestFramework,
+  DUnitX.TestRunner,
+  DUnitX.Loggers.Console,
+  DUnitX.Loggers.XML.NUnit,
+  Poseidon.Tests.WebSocket,
+  Poseidon.Tests.HttpServer,
+  Poseidon.Tests.HTTP2,
+  Poseidon.Tests.Security,
+  Poseidon.Tests.BufferPool,
+  Poseidon.Tests.HTTP1Parser,
+  Poseidon.Tests.ResponseBuilder,
+  Poseidon.Tests.ProxyProtocol,
+  Poseidon.Tests.SSL,
+  Poseidon.Tests.HPACK,
+  Poseidon.Tests.Dispatcher,
+  Poseidon.Tests.Connection,
+  Poseidon.Tests.Workers,
+  Poseidon.Tests.Problem,
+  Poseidon.Tests.Validation,
+  Poseidon.Mock.SSLProvider,
+  Poseidon.Mock.Context;
+
+var
+  LRunner:  ITestRunner;
+  LResults: IRunResults;
+begin
+  try
+    TDUnitX.CheckCommandLine;
+    LRunner := TDUnitX.CreateRunner;
+    LRunner.UseRTTI          := True;
+    LRunner.FailsOnNoAsserts := False;
+    LRunner.AddLogger(TDUnitXConsoleLogger.Create(False));
+    LRunner.AddLogger(TDUnitXXMLNUnitFileLogger.Create('.\bin\DUnitX-Results.xml'));
+    LResults := LRunner.Execute;
+    if not LResults.AllPassed then
+      System.ExitCode := EXIT_ERRORS;
+  except
+    on E: Exception do
+    begin
+      WriteLn(E.ClassName + ': ' + E.Message);
+      System.ExitCode := 1;
+    end;
+  end;
+end.
---
+++ tests/build_tests_core.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+set BDS=C:\Program Files (x86)\Embarcadero\Studio\22.0
+set DCC64="%BDS%\bin\dcc64.exe"
+
+cd /d "D:\IA\Projetos\Delphi\Poseidon\tests"
+
+if not exist dcu\Debug md dcu\Debug
+
+%DCC64% ^
+  --no-config ^
+  -B ^
+  -CC ^
+  "-NSSystem;Xml;Data;Datasnap;Web;Soap;Winapi;System.Win;Data.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win" ^
+  "-U%BDS%\lib\Win64\release;..\src;.\mocks" ^
+  "-I..\src;.\mocks" ^
+  -E. ^
+  "-N0.\dcu\Debug" ^
+  Poseidon.Tests.Core.dpr > build_core_out.txt 2>&1
+
+echo Exit code: %ERRORLEVEL%
